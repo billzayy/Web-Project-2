c3dc9f72fad3b02a61f7d37059553022272dce9d
Tuan Phan Le <coderbillzay@gmail.com>
2023-06-03 21:06:30 +0700

    Add semicolon

--- admin.js
+++ admin.js
@@ -2,7 +2,7 @@ const sql = require('./sql')
 
 function getProduct(req,res) {
     sql.executeSQL(`select * From SanPham`, (recordset) => {
-        res.send(recordset.recordsets[0])
+        res.send(recordset.recordsets[0]);
     }) 
 }
 
